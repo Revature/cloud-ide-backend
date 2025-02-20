02faa3a975025faeaae6cab4226fab1f50727b69
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-02-20 14:03:55 -0500

    update aws.py with EC2 and S3 CRUD

--- app/business/aws.py
+++ app/business/aws.py
@@ -1,6 +1,14 @@
 # aws.py
-
+import asyncio
 import boto3
+from botocore.exceptions import ClientError
+
+ec2 = boto3.client('ec2')
+s3 = boto3.client('s3')
+
+###################
+# EC2 Functionality
+###################
 
 # We can create or terminate vms in batches with the sdk, but I'd recommend that we stick to one at a time for now. If we need to scale, it can be done later, but managing multiple instances across availability zones and regions can lead to inconsistencies, since the termination requests are idempotent by AZ.
 
@@ -43,6 +51,7 @@ async def Stop_EC2(InstanceId) -> str:
     except Exception as e:
         return str(e)   
 
+
 # Start is... well, a startup. Nothing should have changed, but we should probably run a healthcheck on the vm and the runner on it once it's back up, as well as geting the connection details in the event that the IP gets rotated to a new VM while an instance is stopped.
 
 async def Start_EC2(InstanceId) -> str:
@@ -67,3 +76,99 @@ async def Terminate_EC2(InstanceId) -> str:
         return response['TerminatingInstances'][0]['CurrentState']['Name']
     except Exception as e:
         return str(e)
+    
+
+###################
+# S3 Functionality
+###################
+
+async def Create_New_S3_Bucket(BucketName) -> str:
+    try:
+        response = s3.create_bucket(
+            Bucket=BucketName
+            )
+        return response['Location']
+    except Exception as e:
+        return str(e)
+    
+
+async def Delete_S3_Bucket(BucketName) -> str:
+    try:
+        response = s3.delete_bucket(
+            Bucket=BucketName
+            )
+        return response['ResponseMetadata']['HTTPStatusCode']
+    except Exception as e:
+        return str(e)
+    
+
+async def List_S3_Buckets() -> list[str]:
+    try:
+        response = s3.list_buckets()
+        buckets = []
+        for bucket in response['Buckets']:
+            buckets.append(bucket['Name'])
+        return buckets
+    except Exception as e:
+        return [str(e)]
+
+
+# Listing objects may get messy if there are a lot of objects. The sdk is built to handle 1000 objects at a response, with counts for pagination if we need ALL of the objects. I can add that to this method, but I wasn't sure what we were picturing for the scale here.
+async def List_S3_Objects(BucketName) -> list[str]:
+    try:
+        response = s3.list_objects_v2(
+           Bucket=BucketName
+            )
+        objects = []
+        for obj in response['Contents']:
+            objects.append(obj['Key'])
+        return objects
+    except Exception as e:
+        return [str(e)]
+
+
+# Putting objects is both create and update. Puts are never partial, so any metadata change must include the entire object to update
+
+async def Put_S3_Object(BucketName, ObjectName, ObjectData) -> str:
+    try:
+        response = s3.put_object(
+            Bucket=BucketName,
+            Key=ObjectName,
+            Body=ObjectData
+            )
+        return response['ResponseMetadata']['HTTPStatusCode']
+    except Exception as e:
+        return str(e)
+
+
+async def Get_S3_Object(BucketName, ObjectName) -> object:
+    try:
+        response = s3.get_object(
+            Bucket=BucketName,
+            Key=ObjectName
+            )
+        return response['Body'].read()
+    except Exception as e:
+        return str(e)
+
+
+async def Delete_S3_Objects(BucketName, ObjectNames) -> str:
+    for obj in ObjectNames:
+        try:
+            response = s3.delete_object(
+                Bucket=BucketName,
+                Key=obj
+                )
+            return
+        except Exception as e:
+            return str(e)
+
+async def main():
+    print('Running...')
+    res = await Get_S3_Object('revature-dev-01092022','batch_curriculum_v2_contents/0ea30ce73ff6dfeca0878c07cfc9474a.txt')
+    print(res)
+
+asyncio.run(main())
+
+'batch_curriculum_v2_contents/0ea30ce73ff6dfeca0878c07cfc9474a.txt'
+'revature-dev-01092022'
