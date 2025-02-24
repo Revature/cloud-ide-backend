21e33217e35cd847ec687b15a9c619a1f482f593
LiquidPlummer <kyle.plummer@revature.net>
2025-02-24 16:43:52 -0500

    fixed prefix 'app' to 'api'

--- app/api/main.py
+++ app/api/main.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 from app.api.routes import users, runners, machines, images, app_requests # import your new images route
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix="/app/v1/users", tags=["users"])
-api_router.include_router(runners.router, prefix="/app/v1/runners", tags=["runners"])
-# api_router.include_router(scripts.router, prefix="/app/v1/scripts", tags=["scripts"])
-api_router.include_router(images.router, prefix="/app/v1/images", tags=["images"])
-api_router.include_router(machines.router, prefix="/app/v1/machines", tags=["machines"])
-api_router.include_router(app_requests.router, prefix="/app/v1/app_requests", tags=["app_requests"]) # include your new images route
+api_router.include_router(users.router, prefix="/api/v1/users", tags=["users"])
+api_router.include_router(runners.router, prefix="/api/v1/runners", tags=["runners"])
+# api_router.include_router(scripts.router, prefix="/api/v1/scripts", tags=["scripts"])
+api_router.include_router(images.router, prefix="/api/v1/images", tags=["images"])
+api_router.include_router(machines.router, prefix="/api/v1/machines", tags=["machines"])
+api_router.include_router(app_requests.router, prefix="/api/v1/app_requests", tags=["app_requests"]) # include your new images route
