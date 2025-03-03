1dba39759141e5b3ff2815449544b0f417dfcd6a
Ashoka Shringla <ashokashringla@gmail.com>
2025-03-03 15:05:19 -0500

    key management exceptions

--- app/business/key_management.py
+++ app/business/key_management.py
@@ -40,9 +40,9 @@ async def get_daily_key() -> Key:
                 key_record = session.exec(stmt).first()
                 if key_record:
                     return key_record
-            raise Exception from None(f"Key pair already exists in AWS but no record found in DB: {e}")
+            raise Exception(f"Key pair already exists in AWS but no record found in DB: {e}") from None
         else:
-            raise Exception from None(f"Failed to create new key pair: {e}")
+            raise Exception(f"Failed to create new key pair: {e}") from None
 
     if not isinstance(new_keypair, dict):
         raise Exception(f"Failed to create new key pair: {new_keypair}")
