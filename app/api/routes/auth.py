5289f4c36993257f9d9901699b898ace8b35e5cb
LiquidPlummer <kyle.plummer@revature.net>
2025-03-11 15:31:38 -0400

    fixes

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,10 +1,9 @@
 """Authorization route for acquiring bearer tokens."""
 import os
-from pydantic import BaseModel
 from workos import WorkOSClient, exceptions
 from app.api.authentication import password_authentication
 from app.schemas.auth_schema import PasswordAuth
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, HTTPException, Request, Response, status
 
 workos = WorkOSClient(api_key=os.getenv("WORKOS_API_KEY"), client_id=os.getenv("WORKOS_CLIENT_ID"))
 
@@ -16,14 +15,19 @@ def machine_auth(request: Request, passwordAuth: PasswordAuth, response: Respons
     passwordAuth.ip_address = request.client.host
     passwordAuth.user_agent = request.headers.get("User-Agent")
 
+
+
     try:
         access_token = password_authentication(passwordAuth)
         response.headers["Access-Token"] = access_token
-        response.status_code = 200
         return '{"status": 200}'
     except exceptions.BadRequestException:
-        response.status_code = 401
-        return '{"error": "Unauthorized: bad credentials"}'
+        raise HTTPException(status_code=401, detail="Bad Request..")
     except Exception as e:
-        response.status_code = 500
-        return e # TODO Remove me and return something obscure
+        # raise HTTPException(status_code=500, detail="Internal Server Error..")
+        return {
+            "status": "error",
+            "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+            "error": str(e),
+            "error_type": type(e).__name__
+        }
